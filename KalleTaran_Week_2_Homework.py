1345922c7ce4b2b7823391c6aabe3788998cfe4b
Taran Kalle <tkalle34@gmail.com>
2025-07-05 23:35:26 -0700

    Test2

--- KalleTaran_Week_2_Homework.py
+++ KalleTaran_Week_2_Homework.py
@@ -44,6 +44,22 @@ nameList1 = ["Ellie", "Steve", "Sam", "Owen", "Gavin"]
 nameList2 = ["Chris", "Zed", "Mary", "Laura", "Oscar"]
 nameList3 = ["Luis", "Zoe", "Nancy", "Sonita", "Adrien"]
 
+# Problem 1.a
+for i in nameList1:
+    if i[0] <= "L":
+        print(i)
+
+# Problem 1.b
+for i in nameList2:
+    if i[0] >= "M":
+        print(i)
+
+# Problem 1.c
+for i in nameList3:
+    if "K" <= i[0] <= "P":
+        print(i)
+
+
 """
 Problem #2
 (2 pts)
@@ -76,6 +92,19 @@ remember modulus and remainder operations from week 1!
 # 
 num1 = 6541
 num2 = 3289
+
+# Problem 2.a
+print(num1//1000)
+print((num1//100)%10)
+print((num1//10)%10)
+print(num1%10)
+
+# Problem 2.b
+print(num2//1000)
+print((num2//100)%10)
+print((num2//10)%10)
+print(num2%10)
+
 
 """
 Problem #3
@@ -110,6 +139,23 @@ pay(16, 25)
 """
 # Please enter your answers here
 # 
+def pay(wage, hours):
+    payAmount = wage*hours
+    if hours > 40:
+        payAmount = payAmount + ((hours-40)*(wage/2))
+    print(payAmount)
+
+# Problem 3.a
+pay(10,35)
+
+# Problem 3.b
+pay(10, 45)
+
+# Problem 3.c
+pay(12, 50)
+
+# Problem 3.d
+pay(16, 25)
 
 """
 Problem #4
@@ -135,8 +181,35 @@ print()
 
 """
 # Please enter your answers here
-# 
+#
+
+# Problem 4.a
+for i in range(2):
+    print(i, end = " ")
+print()
+
+# Problem 4.b
+for i in range(1):
+    print(i, end = " ")
+print()
+
+# Problem 4.c
 for i in range(4):
+    print(i+3, end = " ")
+print()
+
+# Problem 4.d
+for i in range(1):
+    print(i+1, end = " ")
+print()
+
+# Problem 4.e
+for i in range(2):
+    print(i*3, end = " ")
+print()
+
+# Problem 4.f
+for i in range(5, 25, 4):
     print(i, end = " ")
 print()
 
@@ -166,6 +239,16 @@ result:
 # 
 studentGrades = [[95, 92, 86, 87], [66, 54], [89, 72, 100], [33, 0, 0]]
 
+def getAverage(grades):
+    for i in grades:
+        total = 0
+        for j in i:
+            total = total + j
+        print(total/len(i))
+
+getAverage(studentGrades)
+
+
 """
 BONUS PROBLEM
 EXTRA CREDIT
@@ -187,25 +270,25 @@ you find AND make the appropriate correction!
 
 """
 
-# ## character (name, hitpoints, current status)
-# charList = ("Stanley", 10, "fight")
-# print(charList)
-# def reduceHitPoints(char):
-#     """
-#     reduces the hit points of a character
-#     if character is engaged in a fight
+## character (name, hitpoints, current status)
+charList = ["Stanley", 10, "fight"] # Should be list, not tuple
+print(charList)
+def reduceHitPoints(char):
+    """
+    reduces the hit points of a character
+    if character is engaged in a fight
 
-#     @param char: list[] list of a character stats
-#     """
-#     damage == 2
-#     if char[1] == "fight":
-#         char[1] = char[1] - damage
-#     print(char[0] + ''s hit points are currently ' + str(char[1]))
+    @param char: list[] list of a character stats
+    """
+    damage = 2 # Should be assignment variable not equivalence
+    if char[2] == "fight": # Should check third element, not second
+        char[1] = char[1] - damage
+    print(char[0] + "'s hit points are currently " + str(char[1])) # Double quotes instead of single
 
-# ## reduce hit points of character
-# reduceHitPoints(charList)
-# ## print character with reduced hit points
-# print(charList)
+## reduce hit points of character
+reduceHitPoints(charList)
+## print character with reduced hit points
+print(charList)
 
 """
 END
